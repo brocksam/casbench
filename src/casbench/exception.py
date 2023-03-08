9dd8f5978749614a3386367d6c717e7fc1659473
Sam Brockie <sambrockie@icloud.com>
2023-03-08 16:02:41 +0100

    Add new ExhaustedTokensError

--- src/casbench/exception.py
+++ src/casbench/exception.py
@@ -11,3 +11,15 @@ class InvalidLexemeError(CASBenchError):
     """Raised when a :class:`Lexer` encounters a lexeme that can't be tokenized."""
 
     pass
+
+
+class UnexpectedTokenError(CASBenchError):
+    """Raised when a :class:`Parser` encounters a :class:`Token` that's invalid grammar."""
+
+    pass
+
+
+class ExhaustedTokensError(CASBenchError):
+    """Raised when a :class:`Parser` unexpectedly runs out of :class:`Token` instances."""
+
+    pass
